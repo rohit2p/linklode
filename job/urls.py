a48adf6ad3291f802fe282c123b65cf9a4ff8f23
rohit <rohitprasad20022@gmail.com>
2026-04-17 12:36:39 +0530

    debug remove migrate

--- job/urls.py
+++ job/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from joblink.job import admin
 from . import views
 
 app_name = 'job'
@@ -8,7 +7,6 @@ app_name = 'job'
 urlpatterns = [
     # Public
     path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
     path('jobs/', views.job_list, name='job_list'),
     path('jobs/<int:pk>/', views.job_detail, name='job_detail'),
     path('resources/', views.resource_list, name='resource_list'),
