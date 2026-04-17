924b5d968bcb7e079837206193c9f2d415e4f159
rohit <rohitprasad20022@gmail.com>
2026-04-17 22:01:08 +0530

    fix github actions yaml

--- joblink/urls.py
+++ joblink/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', include('job.urls', namespace='job')),
 ]
 
-# ✅ ONLY in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# # ✅ ONLY in development
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
