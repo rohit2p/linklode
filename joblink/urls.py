ff617862ed7bf5b2ebc343c985f41c503f84f6e2
rohit <rohitprasad20022@gmail.com>
2026-04-17 21:51:12 +0530

    fix github actions yaml

--- joblink/urls.py
+++ joblink/urls.py
@@ -6,4 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('job.urls', namespace='job')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# ✅ ONLY in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
